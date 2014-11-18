30d38c55542e5215ac328c8556e5fdf7acf96c81
Sumeet Agarwal <sumeet.a@gmail.com>
2014-11-17 21:02:45 -0800

    Started simplification of ParamType.
    Probably need to move it closer to the caller and make it not a fake class.

--- lib/schemas/ui/param_type.rb
+++ lib/schemas/ui/param_type.rb
@@ -1,28 +1,13 @@
 module Schemas
   module UI
     class ParamType
-      def initialize(type, is_required, validators)
-        @is_required = is_required
+      def self.new(type, is_required, validators)
         validator_chain = Validators::ValidatorChain.new(validators)
-        @type_with_validator = Types::TypeWithValidator.new(type,
-                                                            validator_chain)
-      end
-
-      def errors(input)
-        type_with_blank_behavior.errors(input)
-      end
-
-      def parse(input)
-        type_with_blank_behavior.parse(input)
-      end
-
-      private
-
-      def type_with_blank_behavior
-        if @is_required
-          Fields::RequiredField.new(@type_with_validator)
+        type_with_validator = Types::TypeWithValidator.new(type, validator_chain)
+        if is_required
+          Fields::RequiredField.new(type_with_validator)
         else
-          Fields::NullWhenBlankField.new(@type_with_validator)
+          Fields::NullWhenBlankField.new(type_with_validator)
         end
       end
     end
